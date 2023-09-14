d74be38c50acf89eb533a193053f8750600b9994
JoannaHerman <92362878+JoannaHerman@users.noreply.github.com>
2023-09-14 12:36:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,37 +19,50 @@ const openModal = function () {
 
 //Showing text from all three modals (show-modal)
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  //uruchomienie funkcji closeModal na X
-  btnCloseModal.addEventListener('click', closeModal);
-  //uruchomienie funkcji przy kliknięciu na overlay
-  overlay.addEventListener('click', closeModal);
   //opening modals
   btnsOpenModal[i].addEventListener('click', openModal);
-  //przeniesione wyżej nad for^
-  // btnsOpenModal[i].addEventListener('click', function () {
-  //   console.log('Button clicked');
-  //   //showing hidden elements[window] (dont use dot)
-  //   modal.classList.remove('hidden');
-  //   overlay.classList.remove('hidden');
-  // });
-  //przeniesione wyżej nad for^
-  /////////DONT REPEAT YOURSELF//////////
-  // btnCloseModal.addEventListener('click', function () {
-  //   //closing window when X is clicked
-  //   modal.classList.add('hidden');
-  //   overlay.classList.add('hidden');
-  // });
-  // overlay.addEventListener('click', function () {
-  //   //closing window when click outside (overlay) window
-  //   modal.classList.add('hidden');
-  //   overlay.classList.add('hidden');
-  // });
-  /////////DONT REPEAT YOURSELF/////
-  //przeniesione wyżej nad for^
-  //  const closeModal = function () {
-  //   modal.classList.add('hidden');
-  //   overlay.classList.add('hidden');
-  // };) //przeniesione wyżej nad for^
 }
+//uruchomienie funkcji closeModal na X
+btnCloseModal.addEventListener('click', closeModal);
+//uruchomienie funkcji przy kliknięciu na overlay
+overlay.addEventListener('click', closeModal);
+//KEYPRESS EVENT
+document.addEventListener('keydown', function (event) {
+  //if modal does nor contain the hidden class then close the modal
+  // if (event.key === 'Escape') {
+  //   if (!modal.classList.contains('hidden')) {
+  //     closeModal();
+  //   }
+  // }
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+//przeniesione wyżej nad for^
+// btnsOpenModal[i].addEventListener('click', function () {
+//   console.log('Button clicked');
+//   //showing hidden elements[window] (dont use dot)
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// });
+//przeniesione wyżej nad for^
+/////////DONT REPEAT YOURSELF//////////
+// btnCloseModal.addEventListener('click', function () {
+//   //closing window when X is clicked
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+// overlay.addEventListener('click', function () {
+//   //closing window when click outside (overlay) window
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+/////////DONT REPEAT YOURSELF/////
+//przeniesione wyżej nad for^
+//  const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };) //przeniesione wyżej nad for^
 
 //exiting modal by keybord(esc)
